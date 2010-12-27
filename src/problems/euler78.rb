acfe9004687bcb21d7704e1c6eda49f7bfa799ff
Benjamin Perrard <ben.perrard@gmail.com>
2010-12-27 14:25:18 +0100

    Problem 78 resolved

--- src/problems/euler78.rb
+++ src/problems/euler78.rb
@@ -1,33 +1,40 @@
-#!/usr/local/bin/ruby
+#!/usr/bin/ruby
 # coding: utf-8
 
 # author:: Benjamin Perrard
-# date:: 17 Décembre 2010
+# date:: 27 Décembre 2010
+
+def pent(value)
+	(value * (3*value-1)) / 2
+end
 
 def main(max)
 	matrix = Array.new
-  matrix[0] = Array.new(1,0)
+  matrix[0] = 1
   i = 0
   loop do
     i+=1
-    matrix[i] = Array.new(i+1,0)
-		matrix[i][1]=1
-		0.upto(i-1) do |j|
-			matrix[i][j] = matrix[i][j-1]
-      #puts "#{i} #{j} #{i-j}"
-      #p matrix
-      matrix[i][j] += matrix[i-j][0]
-      matrix[i-j].delete_at(0)
-		end
-    matrix[i][-1] = matrix[i][-2]+1
-    p i
-    return i if matrix[i][-1] % max == 0
+    matrix[i] = 0
+    sens = 1
+    value = 1
+    while true
+    	tmp = i-(pent(value * sens))
+    	#puts "#{i} #{tmp} #{sens} #{value}"
+    	break if tmp < 0
+    	matrix[i] += ( value % 2 == 0 ? -1 : 1) * matrix[tmp]
+    	if sens == -1
+    		value += 1
+    	end
+    	sens *= -1
+    end
+    matrix[i] = matrix[i] % max
+    #p matrix
+    return i if matrix[i] == 0
 	end
 end
 
 if __FILE__==$0
   max = 1_000_000
-  #max = 7
   time_before = Time.now
   res = main(max)
   puts "Le résultat du problème 78 est #{res}\n"+ \
